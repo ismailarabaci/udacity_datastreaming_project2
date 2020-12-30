d7d76d6f2866d6e477252f6d8953f2100a5a26a3
ismailarabaci <76777209+ismailarabaci@users.noreply.github.com>
2020-12-30 15:20:57 +0100

    Update data_stream.py

--- data_stream.py
+++ data_stream.py
@@ -6,7 +6,7 @@ import pyspark.sql.functions as psf
 
 
 schema = StructType([
-    StructField('crime_id', LongType(), True),
+    StructField('crime_id', StringType(), True),
     StructField('original_crime_type_name', StringType(), True),
     StructField('report_date', TimestampType(), True),
     StructField('call_date', TimestampType(), True),
@@ -36,63 +36,62 @@ def run_spark_job(spark):
     # Show schema for the incoming resources for checks
     df.printSchema()
 
-    # TODO extract the correct column from the kafka input resources
     # Take only value and convert it to String
     kafka_df = df.selectExpr('CAST(value AS STRING)')
 
     service_table = kafka_df\
         .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
         .select("DF.*")
+    
+    service_table.printSchema()
 
-    # TODO select original_crime_type_name and disposition
-    distinct_table = service_table.select('original_crime_type_name', 'disposition').distinct()
+    # select original_crime_type_name and disposition
+    distinct_table = service_table \
+            .select("original_crime_type_name","disposition","call_date_time") \
+            .distinct() \
+            .withWatermark('call_date_time', "1 minute")
 
     # count the number of original crime type
-    agg_df = distinct_table.agg(psf.count('original_crime_type_name'))
+    agg_df = distinct_table \
+    .dropna() \
+    .groupBy("original_crime_type_name").count()
 
-    # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
-    # TODO write output stream
-    query = agg_df \
-                .writeStream \
-                .format('console') \
-                .outputMode('complete') \
-                .start()
+    query = agg_df\
+            .writeStream\
+            .queryName('agg_df') \
+            .format('console') \
+            .outputMode('Complete') \
+            .option("truncate", "false") \
+            .start() \
+            .awaitTermination()
 
-
-    # TODO attach a ProgressReporter
-    query.awaitTermination()
-
-    # TODO get the right radio code json path
     radio_code_json_filepath = "radio_code.json"
     radio_code_df = spark.read.json(radio_code_json_filepath)
 
     # clean up your data so that the column names match on radio_code_df and agg_df
     # we will want to join on the disposition code
-
-    # TODO rename disposition_code column to disposition
     radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition")
 
-    # TODO join on disposition column
-    join_query = agg_df.join(
-        radio_code_df, 
-        agg_df.disposition == radio_code_df.disposition)
-
-
-    join_query.awaitTermination()
-
+    join_query = agg_df \
+                    .join(radio_code_df, col('agg_df.disposition') == col('radio_code_df.disposition')) \
+                    .writeStream \
+                    .queryName('join_query') \
+                    .format('console') \
+                    .outputMode('complete') \
+                    .start() \
+                    .awaitTermination()
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
-    # TODO Create Spark in Standalone mode
     spark = SparkSession \
         .builder \
         .master("local[*]") \
         .appName("KafkaSparkStructuredStreaming") \
+        .config('spark.ui.port', 3000) \
         .getOrCreate()
-
+    
+    spark.sparkContext.setLogLevel('WARN')
     logger.info("Spark started")
-
     run_spark_job(spark)
-
     spark.stop()
